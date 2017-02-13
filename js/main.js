675284616cdf4d4a745322c7f7dda562caf54dbf
Raniro Coelho <errecme@gmail.com>
2017-02-13 15:42:47 -0200

    Set hover skill icons to work outside the svg paths too

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
-var skillIcons = document.getElementsByClassName("main-skills__svg--fill");
+var skillIcons = document.getElementsByClassName("main-skills__svg");
 
 for (var i = 0; i < skillIcons.length; i++) {
   skillIcons[i].onmouseover = function(e) {
-    console.log(this.children);
-    this.classList.remove("main-skills__svg--fill");
+    this.children[0].classList.remove("main-skills__svg--fill");
 };
   skillIcons[i].onmouseout = function(e) {
-    this.classList.add("main-skills__svg--fill");
+    this.children[0].classList.add("main-skills__svg--fill");
 };
 }
