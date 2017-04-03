0cddf8b9d5051fb5618b82bce2b211a5f18496b8
Raniro Coelho <errecme@gmail.com>
2017-04-03 14:45:51 -0300

    Refactor rotate hero: add funtion to manipulate the hero div, set global var, add var tag

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 var skillIcons = document.getElementsByClassName("main-skills__svg");
+var rightHand  = document.getElementById('right-hand');
+var leftHand   = document.getElementById('left-hand');
+var TH = false; // transformHero() flag
 
 for (var i = 0; i < skillIcons.length; i++) {
   skillIcons[i].onmouseover = function(e) {
@@ -10,16 +13,19 @@ for (var i = 0; i < skillIcons.length; i++) {
 }
 
 window.addEventListener('scroll', function(e){
-  rightHand = document.getElementById('right-hand');
-  leftHand = document.getElementById('left-hand');
-  height = window.pageYOffset;
-  if( height > 450 && height < 500) {
-    rightHand.style.transform = 'rotate(-1deg)';
-    rightHand.style.marginTop = '-7%';
-    rightHand.style.marginRight = '-2%';
-    leftHand.style.transform = 'rotate(-15deg)';
-    leftHand.style.marginTop = '-2%';
-    leftHand.style.marginLeft = '-2%';
+  var height     = window.pageYOffset;
+  if( height > 450 && height < 500 && TH === false) {
+    transformHero();
   }
     // console.log(window.pageYOffset);
 }, false);
+
+function transformHero() {
+  TH = true;
+  rightHand.style.transform = 'rotate(-1deg)';
+  rightHand.style.marginTop = '-7%';
+  rightHand.style.marginRight = '-2%';
+  leftHand.style.transform = 'rotate(-15deg)';
+  leftHand.style.marginTop = '-2%';
+  leftHand.style.marginLeft = '-2%';
+}
