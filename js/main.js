615ea0c691bb9aedf6b0c5e909bbd364586aded4
Raniro Coelho <errecme@gmail.com>
2017-04-05 10:35:37 -0300

    Merge checkWorkItensPosition into trnasformWorkGrid function

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
-var skillIcons  = document.getElementsByClassName("main-skills__svg");
-var workItems   = document.getElementsByClassName('main-work__item');
-var rightHand   = document.getElementById('right-hand');
-var leftHand    = document.getElementById('left-hand');
-var heroSection = document.getElementById('hero');
-var workSection = document.getElementById('work');
-var heroYPosition  = heroSection.getBoundingClientRect().top + window.scrollY;
-var workYPosition  = workSection.getBoundingClientRect().top + window.scrollY;
+var skillIcons         = document.getElementsByClassName("main-skills__svg");
+var workItems          = document.getElementsByClassName('main-work__item');
+var rightHand          = document.getElementById('right-hand');
+var leftHand           = document.getElementById('left-hand');
+var heroSection        = document.getElementById('hero');
+var workSection        = document.getElementById('work');
+var heroYPosition      = heroSection.getBoundingClientRect().top + window.scrollY;
+var workYPosition      = workSection.getBoundingClientRect().top + window.scrollY;
 var workItemsYPosition = [];
-var TH          = false; // transformHero() flag
-
-checkWorkItemsYPosition();
+var TH                 = false; // transformHero() flag
 
 for (var i = 0; i < skillIcons.length; i++) {
   skillIcons[i].onmouseover = function(e) {
@@ -23,7 +21,6 @@ for (var i = 0; i < skillIcons.length; i++) {
 window.addEventListener('resize', function(e){
   heroYPosition = heroSection.getBoundingClientRect().top + window.scrollY;
   workYPosition = workSection.getBoundingClientRect().top + window.scrollY;
-  checkWorkItemsYPosition();
 });
 
 window.addEventListener('scroll', function(e){
@@ -39,19 +36,15 @@ window.addEventListener('scroll', function(e){
 
 function transformHero() {
   TH = true;
-  rightHand.style.transform = 'rotate(-1deg)';
-  rightHand.style.marginTop = '-7%';
+  rightHand.style.transform   = 'rotate(-1deg)';
+  rightHand.style.marginTop   = '-7%';
   rightHand.style.marginRight = '-2%';
-  leftHand.style.transform = 'rotate(-15deg)';
-  leftHand.style.marginTop = '-2%';
-  leftHand.style.marginLeft = '-2%';
+  leftHand.style.transform    = 'rotate(-15deg)';
+  leftHand.style.marginTop    = '-2%';
+  leftHand.style.marginLeft   = '-2%';
 }
 
 function transformWorkGrid() {
-  // itemsYPositionFromTop[0] = workSection.getBoundingClientRect().top + window.scrollY;
-}
-
-function checkWorkItemsYPosition() {
   workItemsYPosition = Array.from(workItems).map(function(entry) {
     return document.getElementById(entry.id).getBoundingClientRect().top + window.scrollY;
   });
