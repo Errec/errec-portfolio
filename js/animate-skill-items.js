8b9ebd355e9a4cfef59f1c0c24dfdbffbe91e9ba
Raniro Coelho <errecme@gmail.com>
2017-05-22 22:05:48 -0300

    Refactor animate skill itens code

--- js/animate-skill-items.js
+++ js/animate-skill-items.js
@@ -1,10 +1,21 @@
-var skillIcons         = document.getElementsByClassName("main-skills__svg");
+var svgHover = (function(){
+// Cache the DOM
+var svgUl = document.querySelector("#skills-grid");
+//  Bind mouse events
+svgUl.addEventListener("mouseover", _showColor, false);
+svgUl.addEventListener("mouseout", _removeColor, false);
 
-for (var i = 0; i < skillIcons.length; i++) {
-  skillIcons[i].onmouseover = function(e) {
-    this.children[0].classList.remove("main-skills__svg--fill");
-};
-  skillIcons[i].onmouseout = function(e) {
-    this.children[0].classList.add("main-skills__svg--fill");
-};
+function _showColor(e) {
+  if(e.target !== e.currentTarget) {
+    e.target.classList.remove("main-skills__svg--fill");
+  }
+  e.stopPropagation();
 }
+
+function _removeColor(e) {
+  if(e.target !== e.currentTarget) {
+    e.target.classList.add("main-skills__svg--fill");
+  }
+  e.stopPropagation();
+}
+})();
