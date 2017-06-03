d0b961ea563b999b9340a50105da84be0888476b
Raniro Coelho <errecme@gmail.com>
2017-06-03 18:07:33 -0300

    Set debounce and throttle to transform work grid events

--- js/animate-work-grid.js
+++ js/animate-work-grid.js
@@ -9,8 +9,7 @@ var workGridAnimation = (function(){
   var zeroArr            = new Array(workItems.length+1).join('0').split('').map(parseFloat);
   var TW                 = zeroArr; // transformWorkGrid() flag array
 
-  //  Bind resize window event
-  window.addEventListener('resize', function(e){
+  var _resetItensSize = debounce(function(e){
     workYPosition = workSection.getBoundingClientRect().top + window.scrollY;
     if (window.innerWidth < 720 && (TW.reduce(_add, 0)) !== 0) {
       TW = new Array(workItems.length+1).join('0').split('').map(parseFloat);
@@ -19,28 +18,29 @@ var workGridAnimation = (function(){
         gridItem[i].className = gridItem[i].className.replace(regClassName,' ');
       }
     }
-  });
+  }, 200);
 
-  //  Bind scroll window event
-  window.addEventListener('scroll', function(e){
+  var _transformWorkGrid = throttle(function(e){
     var currentYPosition = window.pageYOffset;
     if (window.innerWidth < 720 && workYPosition - currentYPosition < 50 && (TW.reduce(_add, 0)) < 5) {
-      _transformWorkGrid(currentYPosition);
+      workItemsYPosition = Array.from(workItems).map(function(entry) {
+        return Math.abs(document.getElementById(entry.id).getBoundingClientRect().top + window.scrollY - currentYPosition);
+      }); // Arr with abs values of the distance between every grid item from the top of the window
+      var min = Math.min.apply(null, workItemsYPosition);
+      var divSufix = workItemsYPosition.indexOf(min);
+      var topGridItemID = 'wli-' + divSufix;
+      if (TW[divSufix] !== 1) {
+        document.getElementById(topGridItemID).className += " main-work__item--grow";
+        TW[divSufix] = 1;
+      }
     }
-  }, false);
+  }, 100);
 
-  function _transformWorkGrid(currentYPosition) {
-    workItemsYPosition = Array.from(workItems).map(function(entry) {
-      return Math.abs(document.getElementById(entry.id).getBoundingClientRect().top + window.scrollY - currentYPosition);
-    }); // Arr with abs values of the distance between every grid item from the top of the window
-    var min = Math.min.apply(null, workItemsYPosition);
-    var divSufix = workItemsYPosition.indexOf(min);
-    var topGridItemID = 'wli-' + divSufix;
-    if (TW[divSufix] !== 1) {
-      document.getElementById(topGridItemID).className += " main-work__item--grow";
-      TW[divSufix] = 1;
-    }
-  }
+  //  Bind resize window event
+  window.addEventListener('resize', _resetItensSize);
+
+  //  Bind scroll window event
+  window.addEventListener('scroll', _transformWorkGrid, false);
 
   function _add(a, b) {
       return a + b;
