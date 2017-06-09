5d888ee7422808a76a524050fa53cdfedbd385de
Raniro Coelho <errecme@gmail.com>
2017-06-09 19:00:02 -0300

    Add animatePin function

---
+++ js/animate-footer-pin.js
@@ -0,0 +1,13 @@
+var animatePin = (function() {
+  var pin    = document.getElementById('footer-pin');
+
+  window.addEventListener('scroll', debounce(_checkPin, 50));
+
+  function _checkPin() {
+    var currentPosition = (window.scrollY + window.innerHeight);
+    var totalHeight     = document.documentElement.offsetHeight;
+
+    totalHeight - currentPosition <= 20 ?
+    pin.classList.add('footer__pin--enter') : pin.classList.remove('footer__pin--enter');
+  }
+})();
