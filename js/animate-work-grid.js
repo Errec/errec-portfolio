99d6508a4c6b31fd9d22708b3f5eadeba490aae6
Raniro Coelho <errecme@gmail.com>
2017-07-19 17:48:01 -0300

    Refactor workGridAnimaiont module

--- js/animate-work-grid.js
+++ js/animate-work-grid.js
@@ -2,18 +2,27 @@ var workGridAnimation = (function(){
   // Cache the DOM
   var workItems = document.querySelectorAll('.main-work__item');
 
-  var regClassName = new RegExp('(^| )'+ 'main-work__item--grow' +'($| )','g');
+  var regClassName   = new RegExp('(^| )'+ 'main-work__item--grow' +'($| )','g');
+  var animationCount = 0;
+
+  var _transformWorkGrid = throttle(function() {
+      Array.prototype.forEach.call(workItems, function(workItem) {
+          // top of the div
+        var itemTop = window.scrollY - workItem.offsetTop;
+        if (itemTop > -150 & window.innerWidth < 720 && (workItem.className.indexOf('main-work__item--grow') > -1)) {
+          _removeGrowClass(workItem);
+          if (animationCount >= workItems.length) {
+            window.removeEventListener('scroll', _transformWorkGrid);
+          }
+        }
+      });
+    }, 200);
+
+  var _removeGrowClass = throttle(function(workItem) {
+      workItem.className = workItem.className.replace(regClassName,' ');
+      animationCount++;
+    }, 1000);
 
   //  Bind scroll window event
-  window.addEventListener('scroll', throttle(_transformWorkGrid, 400), false);
-
-  function _transformWorkGrid(){
-    Array.prototype.forEach.call(workItems, function(workItem) {
-        // top of the div
-      var itemTop = window.scrollY - workItem.offsetTop;
-      if (itemTop > -120 & window.innerWidth < 720) {
-        workItem.className = workItem.className.replace(regClassName,' ');
-      }
-    });
-  }
+  window.addEventListener('scroll', _transformWorkGrid, false);
 })();
