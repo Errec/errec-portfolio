aa4e317fe9a8b20e8dc6a6d3b6504b17d88a48ba
Raniro Coelho <errecme@gmail.com>
2017-07-31 16:34:36 -0300

    Remove outline from work items img

--- js/animate-about-bg.js
+++ js/animate-about-bg.js
@@ -1,9 +1,7 @@
 var parallax = (function() {
   var about       = document.getElementById('about');
   var parallaxImg = document.getElementById('about-parallax-img');
-    // half way through the image
   var slideInAt = (window.scrollY + window.innerHeight) - about.height;
-  // bottom of the image
   var imageBottom = about.offsetTop + about.height;
   var isHalfShown = slideInAt > about.offsetTop;
 
