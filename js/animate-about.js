ed91c5830a4a968d238c0d5a32cc190019293045
Raniro Coelho <errecme@gmail.com>
2017-08-01 08:45:15 -0300

    Update about bg animation: only one way , remove event on 100%

--- js/animate-about.js
+++ js/animate-about.js
@@ -3,16 +3,21 @@ var parallax = (function() {
   var about = document.getElementById('about');
   var newTranslate = 0;
 
+  window.addEventListener('scroll', _checkAboutHeight);
+
   function _checkAboutHeight() {
     var slideInAt = (window.scrollY + window.innerHeight) - about.clientHeight / 2;
     var isTopShow = slideInAt > (about.offsetTop);
     if(isTopShow) {
       percentFromBotttom = 200 *  (slideInAt - about.offsetTop) / about.clientHeight;
-      newTranslate = percentFromBotttom;
-      parallaxImg.style.transform = "translate3d(0,-" + newTranslate +"%,0)";
+
+      if (newTranslate < percentFromBotttom) {
+        newTranslate = percentFromBotttom;
+        parallaxImg.style.transform = "translate3d(0,-" + newTranslate +"%,0)";
+      }
+      newTranslate === 100 ? window.removeEventListener('scroll', _checkAboutHeight) : '';
     }
   }
-   window.addEventListener('scroll', _checkAboutHeight);
 })();
 
 
